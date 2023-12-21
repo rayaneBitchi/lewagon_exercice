30e390b1c6a5ecbd47abbdd72b652463854c6c4f
Rayane Bitchi <rayamevoltaire@gmail.com>
2023-12-21 15:52:57 -0500

    Exercice done

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-// You can remove this line when you're ready.
-console.log('Good luck!');
+const formContainer = document.querySelector('.form-container');
+const confirmation = document.querySelector('.confirmation');
+
+const form = document.querySelector('.form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const checkboxInput = document.querySelector('#check');
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (nameInput.value === '' || emailInput.value === '' || checkboxInput.checked === false) {
+    alert('Please fill out all fields and check the box');
+  } else {
+    formContainer.classList.add('hide');
+    confirmation.classList.remove('hide');
+  }
+});
